87a0de9334be0257ee11592db0309bd7798f586a
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-11-17 19:31:38 +0530

    minor changes on importing NewsLetter.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
-import NewsLetter from '../components/newsLetter'
+import NewsLetter from '../components/NewsLetter'
 import Footer from '../components/Footer'
 
 const Home = () => {
